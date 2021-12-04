db53f734d8efd1c28b501c20f3652aff1f1d8e41
Mirco De Zorzi <mircodezorzi@protonmail.com>
2021-12-04 19:41:20 +0100

    refactor(color): cleanup ugly code

--- include/dz/color.h
+++ include/dz/color.h
@@ -1,18 +1,34 @@
 #ifndef __DZ_COLOR_H__
 #define __DZ_COLOR_H__
 
-typedef struct color {
+#include <dz/common.h>
+
+/**
+  \brief RGB color representation.
+
+  \todo Use u32 instead. The 24 least significant bits will be used for respectively r, g, b.
+
+    u32 color_from_rgb(char r, char g, char b) {
+      return r << 16 | g << 8 | b;
+    }
+  */
+typedef struct {
   int r;
   int g;
   int b;
 } color_t;
 
-extern const struct color color_invalid;
+extern const color_t color_invalid;
 
-extern const struct color color_white;
-extern const struct color color_black;
-extern const struct color color_red;
-extern const struct color color_green;
-extern const struct color color_blue;
+extern const color_t color_white;
+extern const color_t color_black;
+extern const color_t color_red;
+extern const color_t color_green;
+extern const color_t color_blue;
+
+extern const color_t color_cyan;
+extern const color_t color_yellow;
+extern const color_t color_purple;
+extern const color_t color_orange;
 
 #endif /* __DZ_COLOR_H__ */
---
+++ src/dz/color.c
@@ -0,0 +1,12 @@
+#include <dz/color.h>
+
+const color_t color_invalid = {-1, -1, -1};
+const color_t color_white   = {255, 255, 255};
+const color_t color_black   = {0, 0, 0};
+const color_t color_red     = {255, 0, 0};
+const color_t color_green   = {0, 255, 0};
+const color_t color_blue    = {0, 0, 255};
+const color_t color_cyan    = {0, 255, 255};
+const color_t color_yellow  = {255, 255, 0};
+const color_t color_purple  = {255, 0, 255};
+const color_t color_orange  = {255, 165, 0};
